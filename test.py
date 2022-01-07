749e0e81de49b486e247f9b92f2f66d819f369aa
Rodrigo Torralbo <rstorralbo@gmail.com>
2022-01-07 16:15:04 -0400

    Alterando o cabecalho

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "DevOps Essentials - 4 Linux")
+        self.assertRegex(result.data.decode(), "Pipeline DevOps - Rodrigo Torralbo")
 
 
 if __name__ == "__main__":
